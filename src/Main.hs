5f8230f08fcbbd019d0b0ec1f8e43514e4148124
Mark Provan <mrp4@st-andrews.ac.uk>
2015-07-20 23:32:10 +0100

    Request reading now works for the most part
    Once read in, requests are now traced to the console. HTTP requests work
    with netcat but not with a web browser - will investigate.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,8 @@
 import Network
 import Control.Concurrent
 import System.IO
-import Debug.Trace
 import Data.Char
+import Debug.Trace
 
 import Server
 
@@ -13,6 +13,7 @@ main = withSocketsDo $ do
 
 loop sock = do
   (handle, hostname, portnumber) <- accept sock
+  hSetBuffering handle NoBuffering
   forkIO $ handleConnection handle
   loop sock
 
@@ -21,20 +22,20 @@ msg = "HTTP/1.0 200 OK\r\nContent-Length: 5\r\n\r\nPong!\r\n"
 handleConnection :: Handle -> IO ()
 handleConnection handle = do
   request <- readRequest handle
-  hPutStr handle msg
+  trace (show request) $ hPutStr handle msg
   hFlush handle
   hClose handle
 
 readRequest :: Handle -> IO Request
 readRequest handle = do
-  incompleteRequest <- readFirstLineOfRequest handle
+  incompleteRequest <- trace ("Reading first line of request") readFirstLineOfRequest handle
   readNextLineOfRequest handle incompleteRequest
 
   where
     readFirstLineOfRequest :: Handle -> IO Request
     readFirstLineOfRequest handle = do
       firstLine <- hGetLine handle
-      let split = words firstLine
+      let split = trace ("First line: " ++ firstLine) $ words firstLine
       let httpMethod = parseHTTPMethod $ split !! 0
       let httpFilename = split !! 1
       let httpVersion = parseHTTPVersion $ split !! 2
@@ -43,11 +44,13 @@ readRequest handle = do
     readNextLineOfRequest :: Handle -> Request -> IO Request
     readNextLineOfRequest handle existingRequest = do
       headerLine <- hGetLine handle
-      let split = splitRequestHeader headerLine
-      let requestFieldName = split !! 0
-      let requestFieldValue = split !! 1
-      let changedRequest = addFieldToRequest existingRequest requestFieldName requestFieldValue
-      return changedRequest
+      case headerLine of
+        "" -> return existingRequest
+        s -> let split = splitRequestHeader s
+                 requestFieldName = split !! 0
+                 requestFieldValue = split !! 1
+                 changedRequest = addFieldToRequest existingRequest requestFieldName requestFieldValue
+             in readNextLineOfRequest handle changedRequest
 
     splitRequestHeader :: String -> [String]
     splitRequestHeader s = case dropWhile isSpace s of
--- src/Server.hs
+++ src/Server.hs
@@ -1,8 +1,11 @@
 module Server where
 
+import Debug.Trace
+
 type THTTPMethod = DHTTPMethod
 data DHTTPMethod = OPTIONS | GET | HEAD | POST
                   | PUT | DELETE | TRACE | CONNECT | PATCH
+    deriving (Show)
 parseHTTPMethod :: String -> THTTPMethod
 parseHTTPMethod string = case string of
   "OPTIONS" -> OPTIONS
@@ -19,6 +22,7 @@ type THTTPFilename = String
 
 type THTTPVersion = DHTTPVersion
 data DHTTPVersion = OneOh | OneOne | TwoOh
+  deriving (Show)
 parseHTTPVersion :: String -> THTTPVersion
 parseHTTPVersion string = case string of
   "HTTP/1.0" -> OneOh
@@ -28,97 +32,178 @@ parseHTTPVersion string = case string of
 type TAccept = String
 initialAccept = ""
 changeRAccept request value = request { rAccept = value }
+
 type TAcceptCharset = String
 initialAcceptCharset = ""
 changeRAcceptCharset request value = request { rAcceptCharset = value }
+
 type TAcceptEncoding = String
 initialAcceptEncoding = ""
 changeRAcceptEncoding request value = request { rAcceptEncoding = value }
+
 type TAcceptLanguge = String
 initialAcceptLanguage = ""
 changeRAcceptLanguage request value = request { rAcceptLanguage = value }
+
 type TAcceptDatetime = String
 initialAcceptDatetime = ""
 changeRAcceptDatetime request value = request { rAcceptLanguage = value }
+
 type TAuthorization = String
 initialAuthorization = ""
 changeRAuthorization request value = request { rAuthorization = value }
+
 type TCacheControl = String
 initialCacheControl = ""
+changeRCacheControl request value = request { rCacheControl = value }
+
 type TConnection = String
 initialConnection = ""
+changeRConnection request value = request { rConnection = value }
+
 type TCookie = String
 initialCookie = ""
+changeRCookie request value = request { rCookie = value }
+
 type TContentLength = String
 initialContentLength = ""
+changeRContentLength request value = request { rContentLength = value }
+
 type TContentMD5 = String
 initialContentMD5 = ""
+changeRContentMD5 request value = request { rContentMD5 = value }
+
 type TContentType = String
 initialContentType = ""
+changeRContentType request value = request { rContentType = value }
+
 type TDate = String
 initialDate = ""
-type TExcept = String
-initialExcept = ""
+changeRDate request value = request { rDate = value }
+
+type TExpect = String
+initialExpect = ""
+changeRExpect request value = request { rExpect = value }
+
 type TFrom = String
 initialFrom = ""
+changeRFrom request value = request { rFrom = value }
+
 type THost = String
 initialHost = ""
+changeRHost request value = request { rHost = value }
+
 type TIfMatch = String
 initialIfMatch = ""
+changeRIfMatch request value = request { rIfMatch = value }
+
 type TIfModifiedSince = String
 initialIfModifiedSince = ""
+changeRIfModifiedSince request value = request { rIfModifiedSince = value }
+
 type TIfNoneMatch = String
 initialIfNoneMatch = ""
+changeRIfNoneMatch request value = request { rIfNoneMatch = value }
+
 type TIfRange = String
 initialIfRange = ""
+changeRIfRange request value = request { rIfRange = value }
+
 type TIfUnmodifiedSince = String
 initialIfUnmodifiedSince = ""
+changeRIfUnmodifiedSince request value = request { rIfUnmodifiedSince = value }
+
 type TMaxForwards = String
 initialMaxForwards = ""
+changeRMaxForwards request value = request { rMaxForwards = value }
+
 type TOrigin = String
 initialOrigin = ""
+changeROrigin request value = request { rOrigin = value }
+
 type TPragma = String
 initialPragma = ""
+changeRPragma request value = request { rPragma = value }
+
 type TProxyAuthorization = String
 initialProxyAuthorization = ""
+changeRProxyAuthorization request value = request { rProxyAuthorization = value }
+
 type TRange = String
 initialRange = ""
+changeRRange request value = request { rRange = value }
+
 type TReferer = String
 initialReferer = ""
+changeRReferer request value = request { rReferer = value }
+
 type TTE = String
 initialTE = ""
+changeRTE request value = request { rTE = value }
+
 type TUserAgent = String
 initialUserAgent = ""
+changeRUserAgent request value = request { rUserAgent = value }
+
 type TUpgrade = String
 initialUpgrade = ""
+changeRUpgrade request value = request { rUpgrade = value }
+
 type TVia = String
 initialVia = ""
+changeRVia request value = request { rVia = value }
+
 type TWarning = String
 initialWarning = ""
+changeRWarning request value = request { rWarning = value }
+
 type TXRequestedWith = String
 initialXRequestedWith = ""
+changeRXRequestedWith request value = request { rXRequestedWith = value }
+
 type TDNT = String
 initialDNT = ""
+changeRDNT request value = request { rDNT = value }
+
 type TXForwardedFor = String
 initialXForwardedFor = ""
+changeRXForwardedFor request value = request { rXForwardedFor = value }
+
 type TXForwardedHost = String
 initialXForwardedHost = ""
+changeRXForwardedHost request value = request { rXForwardedHost = value }
+
 type TXForwardedProto = String
 initialXForwardedProto = ""
+changeRXForwardedProto request value = request { rXForwardedProto = value }
+
 type TFrontEndHttps = String
 initialFrontEndHttps = ""
+changeRFrontEndHttps request value = request { rFrontEndHttps = value }
+
 type TXHttpMethodOverride = String
 initialXHttpMethodOverride = ""
+changeRXHttpMethodOverride request value = request { rXHttpMethodOverride = value }
+
 type TXATTDeviceId = String
 initialXATTDeviceId = ""
+changeRXATTDeviceId request value = request { rXATTDeviceId = value }
+
 type TXWapProfile = String
 initialXWapProfile = ""
+changeRXWapProfile request value = request { rXWapProfile = value }
+
 type TProxyConnection = String
 initialProxyConnection = ""
+changeRProxyConnection request value = request { rProxyConnection = value }
+
 type TXUIDH = String
 initialXUIDH = ""
+changeRXUIDH request value = request { rXUIDH = value }
+
 type TXCsrfToken = String
 initialXCsrfToken = ""
+changeRXCsrfToken request value = request { rXCsrfToken = value }
 
 data Request = Request
   { rHTTPMethod :: THTTPMethod
@@ -137,7 +222,7 @@ data Request = Request
   , rContentMD5 :: TContentMD5
   , rContentType :: TContentType
   , rDate :: TDate
-  , rExcept :: TExcept
+  , rExpect :: TExpect
   , rFrom :: TFrom
   , rHost :: THost
   , rIfMatch :: TIfMatch
@@ -168,7 +253,7 @@ data Request = Request
   , rProxyConnection :: TProxyConnection
   , rXUIDH :: TXUIDH
   , rXCsrfToken :: TXCsrfToken
-  }
+  } deriving (Show)
 
 createFirstLineRequest :: THTTPMethod
                        -> THTTPFilename
@@ -189,7 +274,7 @@ createFirstLineRequest method filename version
    initialContentMD5
    initialContentType
    initialDate
-   initialExcept
+   initialExpect
    initialFrom
    initialHost
    initialIfMatch
@@ -226,6 +311,49 @@ addFieldToRequest :: Request
                   -> String
                   -> Request
 addFieldToRequest existingRequest fieldName fieldValue
- = case fieldName of
+ = case trace ("Adding field " ++ fieldName) fieldName of
    "Accept:" -> changeRAccept existingRequest fieldValue
-   _ -> existingRequest
+   "Accept-Charset:" -> changeRAcceptCharset existingRequest fieldValue
+   "Accept-Encoding:" -> changeRAcceptEncoding existingRequest fieldValue
+   "Accept-Language:" -> changeRAcceptLanguage existingRequest fieldValue
+   "Accept-Datetime:" -> changeRAcceptDatetime existingRequest fieldValue
+   "Authorization:" -> changeRAuthorization existingRequest fieldValue
+   "Cache-Control:" -> changeRCacheControl existingRequest fieldValue
+   "Connection:" -> changeRConnection existingRequest fieldValue
+   "Cookie:" -> changeRCookie existingRequest fieldValue
+   "Content-Length:" -> changeRContentLength existingRequest fieldValue
+   "Content-MD5:" -> changeRContentMD5 existingRequest fieldValue
+   "Content-Type:" -> changeRContentType existingRequest fieldValue
+   "Date:" -> changeRDate existingRequest fieldValue
+   "Expect:" -> changeRExpect existingRequest fieldValue
+   "From:" -> changeRFrom existingRequest fieldValue
+   "Host:" -> changeRHost existingRequest fieldValue
+   "If-Match:" -> changeRIfMatch existingRequest fieldValue
+   "If-Modified-Since:" -> changeRIfModifiedSince existingRequest fieldValue
+   "If-None-Match:" -> changeRIfNoneMatch existingRequest fieldValue
+   "If-Range:" -> changeRIfRange existingRequest fieldValue
+   "If-Unmodified-Since:" -> changeRIfUnmodifiedSince existingRequest fieldValue
+   "Max-Forwards:" -> changeRMaxForwards existingRequest fieldValue
+   "Origin:" -> changeROrigin existingRequest fieldValue
+   "Pragma:" -> changeRPragma existingRequest fieldValue
+   "Proxy-Authorization:" -> changeRProxyAuthorization existingRequest fieldValue
+   "Range:" -> changeRRange existingRequest fieldValue
+   "Referer:" -> changeRReferer existingRequest fieldValue
+   "TE:" -> changeRTE existingRequest fieldValue
+   "User-Agent:" -> changeRUserAgent existingRequest fieldValue
+   "Upgrade:" -> changeRUpgrade existingRequest fieldValue
+   "Via:" -> changeRVia existingRequest fieldValue
+   "Warning:" -> changeRWarning existingRequest fieldValue
+   "X-Requested-With:" -> changeRXRequestedWith existingRequest fieldValue
+   "DNT:" -> changeRDNT existingRequest fieldValue
+   "X-Forwarded-For:" -> changeRXForwardedFor existingRequest fieldValue
+   "X-Forwarded-Host:" -> changeRXForwardedHost existingRequest fieldValue
+   "X-Forwarded-Proto:" -> changeRXForwardedProto existingRequest fieldValue
+   "Front-End-Https:" -> changeRFrontEndHttps existingRequest fieldValue
+   "X-Http-Method-Override:" -> changeRXHttpMethodOverride existingRequest fieldValue
+   "X-ATT-DeviceId:" -> changeRXATTDeviceId existingRequest fieldValue
+   "X-Wap-Profile:" -> changeRXWapProfile existingRequest fieldValue
+   "Proxy-Connection:" -> changeRProxyConnection existingRequest fieldValue
+   "X-UIDH:" -> changeRXUIDH existingRequest fieldValue
+   "X-Csrf-Token:" -> changeRXCsrfToken existingRequest fieldValue
+   _ -> trace ("Could not identify request field " ++ fieldName) existingRequest
